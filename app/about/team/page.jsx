447d327f8afac3f49adaf8b67d0a32c6e561f030
develad <develadb@gmail.com>
2023-04-07 17:45:24 +0300

    Routing System & Nested Routes

---
+++ app/about/team/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+        voluptates est rerum sequi, nisi eveniet sit magni, eos nam qui quasi
+        laudantium esse consectetur ducimus? Ratione quos earum magni, eos
+        ducimus rerum ipsa sunt veritatis adipisci, maiores illum, aspernatur
+        impedit recusandae nobis voluptatem nisi provident? Rem dolorem
+        provident tenetur amet!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Link from "next/link";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>Welcome to our agency media</h1>
+      <ul>
+        <li>
+          <Link href='/'>Home</Link>
+        </li>
+        <li>
+          <Link href='/about'>About</Link>
+        </li>
+        <li>
+          <Link href='/about/team'>About The Team</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
