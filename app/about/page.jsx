d10a1ebb0c071d943249a7b6fdb839015db5b53a
develad <develadb@gmail.com>
2023-04-07 17:51:15 +0300

    meta api

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+export const metadata = {
+  title: "About Agency Media",
+  description: "Web development tutorials and courses",
+  keywords:
+    "web development, web design, javascript, react, node, angular, vue, html, css",
+};
+
 const AboutPage = () => {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe esse
+        maxime quasi aperiam quisquam vel consequuntur itaque optio cupiditate,
+        et sapiente nostrum voluptas ipsa corrupti soluta in quae consequatur,
+        aut dignissimos voluptates. Labore error explicabo odio suscipit rem
+        nostrum, voluptatum quasi repellendus ratione numquam minima omnis fuga
+        perferendis cum rerum, hic adipisci eveniet! Impedit mollitia illo nobis
+        odit in porro.
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;
