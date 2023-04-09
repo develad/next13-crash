0b7ca70594e4a60955a15f869eaac6ac16973af5
develad <develadb@gmail.com>
2023-04-09 14:03:28 +0300

    Getting Body Data

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,25 @@
 import { NextResponse } from "next/server";
 import courses from "./data.json";
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(request) {
+  return NextResponse.json(courses);
+}
+
+export async function POST(request) {
+  const { title, description, level, link } = await request.json();
+
+  //   console.log(title, description, level, link);
+
+  const newCourse = {
+    id: uuidv4(),
+    title,
+    description,
+    link,
+    level,
+  };
+
+  courses.push(newCourse);
+
   return NextResponse.json(courses);
 }
